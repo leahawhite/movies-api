e0160a46a73cf769fda1074ab40e6d45bbe5e0f8
Leah White <leahawhite@gmail.com>
2019-10-22 23:12:31 -0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const app = express()
+
+const morganOption = (process.env.NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello, world')
+})
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at http://localhost:${PORT}`)
+})
