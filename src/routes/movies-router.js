53ffcfeee45575dbdc2df443636288ab5b8faeea
Leah White <leahawhite@gmail.com>
2019-10-23 00:27:01 -0700

    page sort limit

--- src/routes/movies-router.js
+++ src/routes/movies-router.js
@@ -4,14 +4,39 @@ const Movie = require('../models/movie-model')
 moviesRouter
   .route('/')
   .get((req, res, next) => {
-    const { title='' } = req.query
-    Movie.find({ title: new RegExp(title, 'i')})
-    .then(movies => {
-      res
-        .status(200)
-        .json(movies)
-    })
-    .catch(next) 
+    const { title='', sort } = req.query
+    const pageOptions = {
+      page: parseInt(req.query.page) || 0,
+      limit: parseInt(req.query.limit) || 10
+    }
+    // TODO: refine sort if/else, add more options
+    if (sort) {
+      if (!['id', 'title'].includes(sort)) {
+        return res
+          .status(400)
+          .send(`Sort value must be either 'id' or 'title'`)
+      }
+      Movie.find({ title: new RegExp(title, 'i')})
+        .skip(pageOptions.page * pageOptions.limit)
+        .limit(pageOptions.limit)
+        .sort({ [sort]: 'asc' })
+        .then(movies => {
+          res
+            .status(200)
+            .json(movies)
+        })
+        .catch(next) 
+    } else {
+      Movie.find({ title: new RegExp(title, 'i')})
+        .skip(pageOptions.page * pageOptions.limit)
+        .limit(pageOptions.limit)
+        .then(movies => {
+          res
+            .status(200)
+            .json(movies)
+        })
+        .catch(next) 
+      }
   })
   .post((req, res, next) => {
     const { title, release_date, overview } = req.body
